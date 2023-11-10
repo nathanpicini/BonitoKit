166dfa5000e825363f4bc6486abc898f2e07742c
uNathanzin <nathanpicini14@gmail.com>
2023-11-10 14:49:47 -0300

    Initial Commit

---
+++ Sources/BonitoKit/BonitoKit.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+public struct VerdadeView: View {
+    public init(){}
+    public var body: some View {
+        Text("Bonito é legal")
+    }
+}
+
+#Preview{
+    VerdadeView()
+}
